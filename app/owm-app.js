f086e2e4a01859becaec361f9d2220081234fb0c
Tim Sarvey <timsarvey@gmail.com>
2015-11-20 10:02:51 -0500

    Add routes to preset city names. Handle URL errors and redirect.

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,10 +1,30 @@
 angular.module('OWMApp', ['ngRoute'])
-    .config(['$routeProvider', function($routeProvider){
+	.value('owmCities', ['New York', 'Dallas', 'Chicago'])
+    .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/', {
             templateUrl : 'home.html',
             controller : 'HomeCtrl'
-        });
+        }).when('/cities/:city', {
+            templateUrl : 'city.html',
+            controller : 'CityCtrl',
+            resolve : {
+        		city: function(owmCities, $route, $location) {
+        		    var city = $route.current.params.city;
+        		    city = city.replace('_', ' ');
+        		    if(owmCities.indexOf(city) == -1 ) {
+        		        $location.path('/error');
+        		        return;
+        		    }
+        		    return city;
+        		}
+    		}
+        }).when('/error', {
+    		template : '<p>Error - Page Not Found</p>'
+		});
     }])
     .controller('HomeCtrl', function($scope) {
         //empty for now
-    });;
+    })
+    .controller('CityCtrl', function($scope, city) {
+    	$scope.city = city;
+	});
